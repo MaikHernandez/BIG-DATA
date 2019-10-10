71802a88ca98f3857222f7be55f5e4fe64b35495
MaikHernandez <isc.angel.hernandez@gmail.com>
2019-10-09 17:17:37 -0700

    Eval.

--- UNIDAD1/EVALUACION/Evaluacion.scala
+++ UNIDAD1/EVALUACION/Evaluacion.scala
@@ -1,36 +1,35 @@
 //EVALUACION UNIDAD 1
+//Muestras de datos
+val sample0 = List(10,5,20,20,4, 5, 2, 25, 1)
+val sample1 = List(3,4,21,36,10,28,35,5,24,42)
 
-def breakingRecords(score1:List[Int]): Unit={  //(String)
-    var i:Int= 10
-    println("Score: "+ i)
-    //Creacion de variables
-    var min = score1(0)
-    var minCount = 0;
-    var max = score1(0)
-    var maxCount = 0;
+//Función breakingRecords
+def breakingRecords(scores:List[Int]): Unit =
+{
+    var min = scores(0) 
+    var max = scores(0)
+    var cont_min = 0
+    var cont_max = 0
 
-    //Ciclo condicional
-    for(i <-score1){
-        println(" "+ i)
-        if(i<min){
-            min=i
-            minCount+=1
-            println("Minimos: " + min)
+    // Ciclo for para realizar comparativas 
+     for (i <- scores) 
+    {
+        if (i<min)
+        {
+            min = i 
+            cont_min = cont_min +1
         }
-        else if(i > max){
-            max=i
-            maxCount+=1
-            println("Maximos: " + max)
+        else if (i>max) 
+        {
+            max = i 
+            cont_max = cont_max + 1
         }
     }
-    println(score1)
-
-println("Resultados: " + "Maximos: "+ maxCount + " " + "Minimos: " + minCount)
+    println("Output:")
+    println (cont_max, cont_min)
 }
-//Valores de ejemplo
-var score1 = List(10,5,20,20,4,5,2,25,1)
-var score2 = List(3,4,21,36,10,28,35,5,24,42)
 
-//Llamadas a la funcion
-breakingRecords(score1)
-breakingRecords(score2)
+//Llamada para salida
+breakingRecords(sample0)
+
+breakingRecords(sample1)
